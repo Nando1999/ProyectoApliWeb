67db62f0bc9d2b59eaf3cb6488381d2239e0bcef
Nando1999 <fjmasache@espe.edu.ec>
2022-06-04 21:37:50 -0500

    librerias

---
+++ app.py
@@ -0,0 +1,24 @@
+# importar librerias a usar 
+
+from markupsafe import escape
+
+#Importamos librerias Falsk 
+from flask import Flask, abort, render_template
+
+#variable de instancia app
+app=Flask(__name__, template_folder='templates')
+
+#Ruta raíz
+@app.route('/')
+
+#Ruta página de inicio html
+@app.route('/inicio')
+
+#función que retorna la página
+def inicio():
+    return render_template('inicio.html')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+    
