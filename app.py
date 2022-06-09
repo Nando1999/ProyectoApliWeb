7f658ce726f107f64c4dc33fdede6fff3e0fec8b
Nando1999 <fjmasache@espe.edu.ec>
2022-06-09 03:04:46 -0500

    subir cambios

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # importar librerias a usar 
 
+from urllib import request
 from markupsafe import escape
 
 #Importamos librerias Falsk 
@@ -13,14 +14,43 @@ app=Flask(__name__, template_folder='templates')
 
 #función que retorna la página
 def principal():
-    return render_template('login.html')
+    return render_template('principal.html')
 
-#Ruta página de zapatos html
-@app.route('/principal.html')
+#-----------------------------------------
+
+#Ruta página de inicio html
+@app.route('/donaciones.html')
 
 #función que retorna la página
-def contactos():
-    return render_template('principal.html')    
+def donaciones():
+    return render_template('donaciones.html')  
+#----------------------------
+#Ruta página de inicio html
+@app.route('/donaciones.html')
+ 
+    
+@app.route('/login.html')
+
+#función que retorna la página
+def login():
+    return render_template('login.html')     
+
+@app.route('/contactos.html')
+
+#función que retorna la página
+def donaciones1():
+    return render_template('contactos.html')       
+          
+#----------------------------------------------
+
+#Ruta página de inicio html
+@app.route('/pagotarjeta.html')
+
+#función que retorna la página
+def pagotarjeta():
+    return render_template('pagotarjeta.html') 
+
+
 
 
 if __name__=='__main__':
